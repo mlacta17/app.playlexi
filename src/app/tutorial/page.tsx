aa154e283047afb4234d7cb6fae4869bde4eaf83
lactao.maria04 <lactao.maria04@gmail.com>
2025-12-11 01:21:25 -0500

    "fixed page.tsx from claude ai's code"

--- src/app/tutorial/page.tsx
+++ src/app/tutorial/page.tsx
@@ -9,60 +9,6 @@ import Voice from "@/components/ui/voice"
 import { CircleDashedIcon } from "lucide-react";
 
 export default function TutorialPage() {
-    return (
-      <div className="p-6">
-        <h1 className="text-2xl font-bold">Welcome to the Tutorial - working as button proxy</h1>
-
-        {/* Default */}
-        <Button>Default Button</Button>
-
-        {/* Variants */}
-        <br/>
-        <Button variant = "secondary">Secondary</Button>
-        <br/>
-        <Button variant = "destructive">Destructive</Button>
-        <br/>
-        <Button variant = "ghost">Ghost</Button>
-        <br/>
-        <Button variant = "outline">Outline</Button>
-        <br/>
-        <Button variant = "link">Link</Button>
-        <br/>
-        <Button variant = "default" size = "icon" ><CircleDashedIcon /></Button>
-        <br/>
-
-        {/*Input field*/}
-        <br/>
-        <Input type="test" placeholder="Input field" />
-        <br/>
-
-        <br/>
-        {/*Progress Bar */}
-        <Progress value={33} />
-        <br/>
-
-        {/*Tabs */}
-        <Tabs defaultValue="account" className="w-[400px]">
-          <TabsList>
-            <TabsTrigger value="account">Account</TabsTrigger>
-            <TabsTrigger value="password">Password</TabsTrigger>
-          </TabsList>
-          <TabsContent value="account">Make changes to your account here</TabsContent>
-          <TabsContent value="password">Change your password here</TabsContent>
-        </Tabs>
-
-        <br/>
-
-        {/* Avatars */}
-        <Avatar>
-          <AvatarImage src="https://github.com/shadcn.png" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
-
-        <br/>
-        {/* Voice input */}
-
-export default function TutorialPage() {
   const handleRecord = () => {
     console.log("Record clicked")
   }
