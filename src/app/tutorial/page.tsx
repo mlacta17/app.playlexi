bbb8c26709ea06b64e58c7f9c4deb2c40631cf7b
lactao.maria04 <lactao.maria04@gmail.com>
2025-12-11 00:12:52 -0500

    "changed tabs and added tabs to page.tsx"

--- src/app/tutorial/page.tsx
+++ src/app/tutorial/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Progress } from "@/components/ui/progress"; 
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { CircleDashedIcon } from "lucide-react";
 
 export default function TutorialPage() {
@@ -34,6 +35,20 @@ export default function TutorialPage() {
         <br/>
         {/*Progress Bar */}
         <Progress value={33} />
+        <br/>
+
+        {/*Tabs */}
+        <Tabs defaultValue="account" className="w-[400px]">
+          <TabsList>
+            <TabsTrigger value="account">Account</TabsTrigger>
+            <TabsTrigger value="password">Password</TabsTrigger>
+          </TabsList>
+          <TabsContent value="account">Make changes to your account here</TabsContent>
+          <TabsContent value="password">Change your password here</TabsContent>
+
+
+        </Tabs>
+
 
       </div>
     );
